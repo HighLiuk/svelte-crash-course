008af2b69b972ff48f96db5eba04870a8cd806d2
Luca Di Fazio <highliuk@gmail.com>
2022-07-12 17:52:05 +0200

    ♻ User Component

--- src/lib/Users.svelte
+++ src/lib/Users.svelte
@@ -2,6 +2,7 @@
   import user1 from "../assets/img/user1.png"
   import user2 from "../assets/img/user2.png"
   import user3 from "../assets/img/user3.png"
+  import User from "./User.svelte"
 
   const getUsers = () => [
     {
@@ -28,18 +29,8 @@
 <div>
   <h1 class="text-2xl text-center mt-10">List of Users</h1>
 
-  {#each getUsers() as { name, email, image, id }, i (id)}
-    <div
-      class="flex m-5 bg-white border p-2 rounded-lg shadow-md hover:shadow-lg cursor-pointer items-center"
-    >
-      <p class="w-10">{i + 1}</p>
-
-      <img class="rounded-full w-16 h-16 mx-4" src={image} alt={name} />
-
-      <p class="mx-4 w-20">{name}</p>
-
-      <p class="mx-4 w-20">{email}</p>
-    </div>
+  {#each getUsers() as user, i (user.id)}
+    <User {user} {i} />
   {:else}
     <p>No users found</p>
   {/each}
