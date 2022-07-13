b177a8081c8fc0ea48450718a64fbc241fdec507
Luca Di Fazio <highliuk@gmail.com>
2022-07-13 16:12:35 +0200

    ✨ Progress bar and Tweened

--- src/lib/Users.svelte
+++ src/lib/Users.svelte
@@ -1,8 +1,11 @@
 <script>
+  import { cubicOut } from "svelte/easing"
+  import { tweened } from "svelte/motion"
   import FilterUser from "./FilterUser.svelte"
   import NewUser from "./NewUser.svelte"
   import User from "./User.svelte"
   import { add, remove, users } from "../store"
+  import { onMount } from "svelte"
 
   let status = "all"
 
@@ -12,6 +15,10 @@
       : $users.filter((user) => user.active === (status === "active"))
 
   const filter = ({ detail }) => (status = detail)
+
+  const progress = tweened(0, { easing: cubicOut, duration: 1000 })
+
+  onMount(() => progress.set(filteredUsers.length))
 </script>
 
 <div class="container mx-auto">
@@ -22,6 +29,8 @@
 
     <NewUser on:newUser={add} />
   </div>
+
+  <progress class="w-11/12 mx-auto block" min="0" max="10" value={$progress} />
 
   {#each filteredUsers as user, i (user.id)}
     <User {user} {i} on:remove={remove} />
