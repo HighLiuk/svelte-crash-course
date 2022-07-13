9797bb021d363b223fa7488943f06c0da4dd6943
Luca Di Fazio <highliuk@gmail.com>
2022-07-13 12:52:58 +0200

    ✨ Slots

--- src/lib/Modal.svelte
+++ src/lib/Modal.svelte
@@ -21,12 +21,18 @@
       <div
         class="relative bg-white rounded-lg px-4 pb-4 text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:max-w-sm sm:w-full sm:p-6"
       >
-        <div class="mt-5 sm:mt-6 text-center">
-          <button
-            class="px-2 py-1 bg-white border rounded hover:bg-gray-200"
-            on:click={closeModal}>Close</button
-          >
-        </div>
+        <form class="mt-5 sm:mt-6">
+          <slot />
+
+          <div class="flex justify-between">
+            <button
+              class="px-2 py-1 bg-white border rounded hover:bg-gray-200"
+              on:click={closeModal}>Close</button
+            >
+
+            <slot name="right-button" />
+          </div>
+        </form>
       </div>
     </div>
   </div>
--- src/lib/NewUser.svelte
+++ src/lib/NewUser.svelte
@@ -11,6 +11,14 @@
   >
 
   {#if showModal}
-    <Modal on:close={() => (showModal = false)} />
+    <Modal on:close={() => (showModal = false)}>
+      <h1 class="text-2xl text-center">Create new user</h1>
+
+      <button
+        class="px-2 py-1 bg-blue-800 text-white rounded-lg"
+        slot="right-button"
+        type="submit">Create</button
+      >
+    </Modal>
   {/if}
 </div>
